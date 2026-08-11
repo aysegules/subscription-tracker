a084b3f33791e087c457d20553355880da11026a
aysegul <ayseguleskk@gmail.com>
2026-08-11 16:24:40 +0300

    removed unused routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,7 @@
 import { Router } from "express";
+import { getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/", () => {});
-userRouter.get("/:id", () => {});
-userRouter.post("/", () => {});
-userRouter.put("/:id", () => {});
-userRouter.delete("/:id", () => {});
-
+userRouter.get("/", getUsers);
 export default userRouter;
