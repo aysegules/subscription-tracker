9e44b5948b28639fd9e6f6abf76e74083ab48b76
aysegul <ayseguleskk@gmail.com>
2026-08-12 16:45:13 +0300

    add sendReminderEmail utility and fix reminder logic in sendReminders function

--- src/controllers/workflow.controller.js
+++ src/controllers/workflow.controller.js
@@ -1,6 +1,7 @@
 import { serve } from "@upstash/workflow/express";
 import { prisma } from "../../lib/prisma.ts";
 import dayjs from "dayjs";
+import { sendReminderEmail } from "../utils/sendEmail.js";
 
 const REMINDERS = [7, 5, 2, 1];
 
@@ -20,16 +21,53 @@ const sendReminders = serve(async (context) => {
     return;
   }
 
-  for (const deaysBefore of REMINDERS) {
-    const reminderDate = renewalDate.subtract(deaysBefore, "day");
+  for (const daysBefore of REMINDERS) {
+    const reminderDate = renewalDate.subtract(daysBefore, "day");
+
+    if (reminderDate.isAfter(dayjs())) {
+      await sleepUntilReminder(
+        context,
+        `${daysBefore} days before reminder`,
+        reminderDate,
+      );
+    }
+
+    if (dayjs().isSame(reminderDate, "day")) {
+      await triggerReminder(
+        context,
+        `${daysBefore} days before reminder`,
+        subscription,
+      );
+    }
   }
 });
 
 const fetchSubscription = async (context, subscriptionId) => {
-  return await context.run("get subscription", () => {
+  return await context.run("get subscription", async () => {
     return prisma.subscription.findUnique({
       where: { id: subscriptionId },
-      include: { user: true },
+      include: {
+        user: {
+          select: { email: true, name: true },
+        },
+      },
+    });
+  });
+};
+
+const sleepUntilReminder = async (context, label, date) => {
+  console.log(`Sleepind until ${label} reminder at ${date}`);
+
+  await context.sleepUntil(label, date.toDate());
+};
+
+const triggerReminder = async (context, label, subscription) => {
+  return await context.run(label, async () => {
+    console.log(`Triggering ${label} reminder`);
+    await sendReminderEmail({
+      to: subscription.user.email,
+      type: label,
+      subscription,
     });
   });
 };
