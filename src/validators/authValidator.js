eddaa8cf39579c4336c6650faee35495ae839271
aysegul <ayseguleskk@gmail.com>
2026-08-12 16:46:43 +0300

    refactor: update authValidator to use ES module syntax and include login schema

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -1,9 +1,14 @@
-const { default: z } = require("zod");
+import { z } from "zod";
 
 const registerSchema = z.object({
   name: z.string().trim().min(2).max(50),
-  email: z.email().string().trim().min(5).max(255).toLowerCase(),
-  password:z.string().min(6)
+  email: z.string().trim().min(5).max(255).toLowerCase().email(),
+  password: z.string().min(6),
 });
 
-export { registerSchema };
+const loginSchema = z.object({
+  email: z.string().trim().toLowerCase().min(2).max(255).email(),
+  password: z.string().min(6),
+});
+
+export { registerSchema, loginSchema };
